3bf2675f185a3a6b7969bcc95b0e93e4a81f06f5
Leonard König <leonard.r.koenig@googlemail.com>
2018-09-29 18:49:13 +0200

    Misc
     - Move io_execx below io_readex, also guard by SOFTMMU_CODE_ACCESS
     - Register cpu as realized after creation, otherwise device paths would
       be NULL
     - Don't vmstate_register, this should've been taken care of by the init
       functions
     - AIC: Restructure, QOM-ify more
     - restore needs a new param now

--- target/arm/helper.c
+++ target/arm/helper.c
@@ -11339,6 +11339,14 @@ static bool get_phys_addr_lpae(CPUARMState *env, uint64_t address,
             goto do_fault;
         }
     }
+<<<<<<< HEAD
+=======
+    
+    cpu_restore_state(cs, GETPC(), true /* XXX should TB be interrupted? */);
+    cpu_loop_exit(cs);
+}
+#endif
+>>>>>>> Misc
 
     if (param.using64k) {
         stride = 13;
